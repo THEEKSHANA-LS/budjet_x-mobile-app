99fcac12ea2d84f18da023cbd2ff6f4e33691b1a
THEEKSHANA L.S. <stheekshana90@gmail.com>
2026-03-20 23:15:21 +0530

    start develop user data screen

--- lib/screens/user_data_screen.dart
+++ lib/screens/user_data_screen.dart
@@ -1,3 +1,5 @@
+import 'package:budjet_x/constants/colors.dart';
+import 'package:budjet_x/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class UserDataScreen extends StatefulWidget {
@@ -10,6 +12,99 @@ class UserDataScreen extends StatefulWidget {
 class _UserDataScreenState extends State<UserDataScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("User Data Screen")));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Enter your \nPersonal Details",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 30),
+
+                //Form...
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Form field for the user name...
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: const EdgeInsets.all(20),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+
+                      //Form field for the user email...
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: const EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 15),
+                      //form field for user password...
+                      TextFormField(
+                        obscureText:
+                            true, //hide what user type in the text field...
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: const EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 15),
+                      //form field for user confirm password...
+                      TextFormField(
+                        obscureText:
+                            true, //hide what user type in the text field...
+                        decoration: InputDecoration(
+                          hintText: "Confirm Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: const EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 30),
+
+                      //remember me for the next time...
+                      Row(
+                        children: [
+                          const Text(
+                            "Remember me for the next time",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: kGrey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
