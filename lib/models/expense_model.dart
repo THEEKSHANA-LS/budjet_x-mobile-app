e1270cb817f54a10521786503991f748055223d7
THEEKSHANA L.S. <stheekshana90@gmail.com>
2026-04-05 01:17:07 +0530

    add json serialization and deserialization

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -44,5 +44,33 @@ class Expense {
     required this.category, 
     required this.date, 
     required this.time, 
-    required this.description});
+    required this.description}
+  );
+
+  //Convert the expense object to a json object...(serialization)
+  Map < String , dynamic > toJSON(){
+    return{
+      'id' : id,
+      'title' : title,
+      'amount' : amount,
+      'category' : category.index,
+      'date' : date.toIso8601String(),
+      'time' : time.toIso8601String(),
+      'description' : description,
+    };
+  }
+
+  //Create an expense object from a JSON object...(Deserialization)
+  factory Expense.fromJSON(Map<String, dynamic> json){
+    return Expense(
+      id: json['id'],
+      title : json['title'],
+      amount: json['amount'],
+      category: ExpenseCategory.values[json['category']],
+      date : DateTime.parse(json['date']),
+      time : DateTime.parse(json['time']),
+      description: json['description'],
+    );
+  }
+
 }
--- lib/models/income_model.dart
+++ lib/models/income_model.dart
@@ -37,4 +37,30 @@ class Income {
     required this.time,
     required this.description,
   });
+
+  //Convert the income object to a json object...(serialization)
+  Map < String , dynamic > toJSON(){
+    return{
+      'id' : id,
+      'title' : title,
+      'amount' : amount,
+      'category' : category.index,
+      'date' : date.toIso8601String(),
+      'time' : time.toIso8601String(),
+      'description' : description,
+    };
+  }
+
+  //Create an income object from a JSON object...(Deserialization)
+  factory Income.fromJSON(Map<String, dynamic> json){
+    return Income(
+      id: json['id'],
+      title : json['title'],
+      amount: json['amount'],
+      category: IncomeCategory.values[json['category']],
+      date : DateTime.parse(json['date']),
+      time : DateTime.parse(json['time']),
+      description: json['description'],
+    );
+  }
 }
